71a3841394c7e6896c1b2d285298ac88e162fe48
arthurchagas1 <128742361+arthurchagas1@users.noreply.github.com>
2024-08-16 14:49:43 -0300

    Create 2791.c

---
+++ beecrowd/2791.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int c1, c2, c3, c4;
+    
+    scanf("%d%d%d%d", &c1, &c2, &c3, &c4);
+    
+    if(c1 == 1) {
+        puts("1");
+    }
+    if(c2 == 1) {
+        puts("2");
+    }
+    if(c3 == 1) {
+        puts("3");
+    }
+    if(c4 == 1) {
+        puts("4");
+    }
+ 
+    return 0;
+}
