326ff6a48c6d6a7fbc2dbb62c805fb83548d1294
arthurchagas1 <128742361+arthurchagas1@users.noreply.github.com>
2024-08-16 14:57:00 -0300

    Create 1010.c

---
+++ beecrowd/1010.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int codigo1, numero1, codigo2, numero2;
+    double valor1, valor2;
+    
+    scanf("%d%d%lf", &codigo1, &numero1, &valor1);
+    scanf("%d%d%lf", &codigo2, &numero2, &valor2);
+    
+    double total;
+    
+    total = (numero1 * valor1) + (numero2 * valor2);
+    
+    printf("VALOR A PAGAR: R$ %.2lf", total);
+    printf("\n");
+ 
+    return 0;
+}
