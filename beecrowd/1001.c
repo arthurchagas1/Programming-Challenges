e7e8c64e3a72924816e709fc08314544185b9858
arthurchagas1 <128742361+arthurchagas1@users.noreply.github.com>
2024-08-16 14:48:47 -0300

    Create 1001.c

---
+++ beecrowd/1001.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int a, b;
+    
+    scanf("%d%d", &a, &b);
+    
+    int soma;
+    
+    soma = a + b;
+    
+    printf("X = %d\n", soma);
+ 
+    return 0;
+}
