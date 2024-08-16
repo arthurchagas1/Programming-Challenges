1c5a4c9443cbb7814264bf52bddae2f80df0fbfb
arthurchagas1 <128742361+arthurchagas1@users.noreply.github.com>
2024-08-16 14:55:24 -0300

    Create 2374.cpp

---
+++ beecrowd/2374.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int n1, n2;
+    
+    scanf("%d%d", &n1, &n2);
+    
+    int total;
+    total = n1 - n2;
+    
+    printf("%d\n", total);
+ 
+    return 0;
+}
