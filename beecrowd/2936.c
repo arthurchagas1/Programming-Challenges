bff85fee985dea67c83968c95f13701718911530
arthurchagas1 <128742361+arthurchagas1@users.noreply.github.com>
2024-08-16 14:49:09 -0300

    Create 2936.c

---
+++ beecrowd/2936.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int a, b, c, d, e;
+    
+    scanf("%d%d%d%d%d", &a, &b, &c, &d, &e);
+    
+    int total;
+    
+    total = a*300 + b*1500 + c*600 + d*1000 + e*150 + 225;
+    
+    printf("%d\n", total);
+ 
+    return 0;
+}
