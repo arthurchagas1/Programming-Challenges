e62ab4701834b651643e9024511ac3c52c3d2723
arthurchagas1 <128742361+arthurchagas1@users.noreply.github.com>
2024-08-16 14:55:51 -0300

    Create 2413.c

---
+++ beecrowd/2413.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int n1, n2;
+    
+    scanf("%d", &n1);
+    
+    n2 = n1*4;
+    
+    printf("%d", n2);
+    
+    printf("\n");
+ 
+    return 0;
+}
