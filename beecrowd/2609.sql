0378c9a58572b56fb1f9d2bcc96d88273821b313
arthurchagas1 <128742361+arthurchagas1@users.noreply.github.com>
2024-08-17 16:06:33 -0300

    Create 2609.sql

---
+++ beecrowd/2609.sql
@@ -0,0 +1,11 @@
+SELECT 
+    categories.name AS category_name, 
+    SUM(products.amount) AS total_amount
+FROM 
+    products
+JOIN 
+    categories ON products.id_categories = categories.id
+GROUP BY 
+    categories.name
+ORDER BY 
+    category_name;
