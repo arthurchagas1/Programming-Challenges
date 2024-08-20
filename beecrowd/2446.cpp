9d9c6ec796c580fca53a549ae9b1e45f3c3df04b
arthurchagas1 <128742361+arthurchagas1@users.noreply.github.com>
2024-08-20 17:14:53 -0300

    Create 2446.cpp

---
+++ beecrowd/2446.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int V, M;
+    cin >> V >> M;
+
+    vector<int> moedas(M);
+    for (int i = 0; i < M; ++i) {
+        cin >> moedas[i];
+    }
+
+    vector<bool> dp(V + 1, false);
+    dp[0] = true;
+
+    for (int i = 0; i < M; ++i) {
+        for (int j = V; j >= moedas[i]; --j) {
+            if (dp[j - moedas[i]]) {
+                dp[j] = true;
+            }
+        }
+    }
+
+    if (dp[V]) {
+        cout << "S" << endl;
+    } else {
+        cout << "N" << endl;
+    }
+
+    return 0;
+}
